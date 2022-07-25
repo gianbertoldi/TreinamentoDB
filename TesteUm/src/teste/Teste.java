fad73c212e5f84f0193e7c56e3a894915b28d7dc
Gian Bertoldi <bertoldi555@icloud.com>
2022-07-25 16:42:26 -0300

    Mostrando

--- TesteUm/src/teste/Teste.java
+++ TesteUm/src/teste/Teste.java
@@ -1,37 +1,11 @@
 package teste;
 
-import javax.swing.JOptionPane;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String menu;
-
-		JOptionPane.showConfirmDialog(null, "Iniciando programa", null, 0);
-		
-		menu = JOptionPane.showInputDialog(null, "Qual a opçao do programa que ira iniciar \n digite o numero "
-				+ "correspondente \n"
-				+ "1 = ola mundo \n"
-				+ "2 = teste menu \n"
-				+ "0 = finalizar o programa");
-		
-		while(menu != "0") {
-			switch (menu) {
-			case "1":
-				JOptionPane.showInternalMessageDialog(null, "Ola Mundo");
-				break;
-			case "2":
-				JOptionPane.showInternalMessageDialog(null, "teste menu");
-				break;
-			default:
-					JOptionPane.showInternalMessageDialog(null, "escolha apenas os numeros disponiveis na tela");
-			break;
-			}
-			
-		}
-		//JOptionPane.showInternalMessageDialog(null, "Ola Mundo");
-		//System.out.println("Ola mundo");
+		System.out.println("ola mundo!");
+		System.out.println("Olha so");
 		
 	}
 
